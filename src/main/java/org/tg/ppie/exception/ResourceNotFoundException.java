6ce3b96449451bfa72638698a8fa8886da9f3cbb
SudheerPothurai <Sudheer.Pothurai@target.com>
2021-06-02 15:38:47 +0530

    README.md and Junit test cases added

--- src/main/java/org/tg/ppie/exception/ResourceNotFoundException.java
+++ src/main/java/org/tg/ppie/exception/ResourceNotFoundException.java
@@ -3,12 +3,18 @@ package org.tg.ppie.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Set;
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends Exception{
+public class ResourceNotFoundException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
-    public ResourceNotFoundException(String message){
+    public ResourceNotFoundException(String message) {
         super(message);
+    }
+
+    public ResourceNotFoundException(Set<String> keys) {
+        super("Field " + keys.toString() + " update is not allow.");
     }
 }
---
+++ src/main/resources/employee_data.sql
@@ -0,0 +1,14 @@
+create table employees(
+id int(10) not null AUTO_INCREMENT,
+first_name varchar(50) ,
+last_name varchar(50) ,
+age int(3) ,
+email_address varchar(100) ,
+PRIMARY KEY (id),
+UNIQUE KEY(email_address)
+);
+insert into employees(first_name,last_name,age,email_address) values ('Sudheer','P','32','sudheer@pie.com');
+insert into employees(first_name,last_name,age,email_address) values ('Waseem','A','30','waseem@pie.com');
+insert into employees(first_name,last_name,age,email_address) values ('Nitin','K','32','nitin@pie.com');
+insert into employees(first_name,last_name,age,email_address) values ('Manju','G','38','manju@pie.com');
+insert into employees(first_name,last_name,age,email_address) values ('Sunil','R','33','sunil@pie.com');
